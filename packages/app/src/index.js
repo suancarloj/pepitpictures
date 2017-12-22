e9bbeec64b3dbc0479fa5148f4bbbfb05cbc145d
Juan Carlos Sanchez Cardenas <juan@qover.me>
2017-12-22 12:18:05 +0100

    Move file

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles';
-import App from './App';
-import './App.css';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/app';
+import './containers/app/App.css';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
