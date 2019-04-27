daaa677e72edc52f0a14ab74f8a814508a973115
Juan Carlos Sanchez Cardenas <juan@qover.me>
2019-04-27 22:44:32 +0200

    Add socket topic for new fresh picture set

--- server/appSocket.js
+++ server/appSocket.js
@@ -1,17 +1,20 @@
 const PicturesService = require('./services/picturesService');
 module.exports = function(socket) {
   let { room } = socket.handshake.query;
-  console.log('join:', room)
+  console.log('join:', room);
   socket.join(room);
   socket.on('show-email-popup', (data) => {
-    console.log('show-email-popup:', data)
-    socket.to(room).emit('show-email-popup', data)
+    console.log('show-email-popup:', data);
+    socket.to(room).emit('show-email-popup', data);
   });
   socket.on('live-email-change', (data) => {
-    console.log('live-email-change:', data)
-    PicturesService.updateEmail(data.pictureSetId, data.email)
-      .then(set => {
-        socket.to(room).emit('live-email-change', data.email)
-      });
+    console.log('live-email-change:', data);
+    PicturesService.updateEmail(data.pictureSetId, data.email).then((set) => {
+      socket.to(room).emit('live-email-change', data.email);
+    });
+  });
+  socket.on('fetch-fresh-data', (data) => {
+    console.log('fetch-fresh-data:', data);
+    socket.to(room).emit('fetch-fresh-data', data);
   });
 };
