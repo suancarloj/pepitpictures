80ffaf3167938c8bcb0bed7373a1f9de61eea8b2
Juan Carlos Sanchez Cardenas <juan@qover.me>
2019-05-01 11:01:47 +0200

    Add missing email

--- picture-viewer/src/pages/view/Viewer.js
+++ picture-viewer/src/pages/view/Viewer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { PictureViewer } from './PictureViewer';
 import { Navbar } from './Navbar';
+import Email from './Email';
 import Socket from '../../components/Socket';
 import { basePath, getAllPictures, starPicture } from '../../services/collection';
 
@@ -34,46 +35,53 @@ export function Viewer(props) {
   const picture = collection[currentPicture];
 
   return (
-    <Socket
-      host={`${basePath}?room=computer-${props.computerId}`}
-      registerId={props.computerId}
-      subscribeTopic="fetch-fresh-data"
-      subscribeCallback={(topic, data) => updateCollection()}
-    >
-      {() => (
-        <>
-          <PictureViewer imgSrc={picture && picture.name} />
-          <Navbar
-            next={next}
-            pictureCount={pictureCollection.pictures.length}
-            prev={prev}
-            showStared={showStared}
-            stared={picture && picture.stared}
-            staredCount={staredCollection.filter((p) => p.stared).length}
-            toggleStarPicture={() => {
-              starPicture({ setId: pictureSetId, pictureId: picture._id, stared: !picture.stared })
-                .then(() => {
-                  if (picture.stared) {
-                    const nextPictureIndex = currentPicture - 1;
-                    if (nextPictureIndex > 0) {
-                      setCurrentPicture(nextPictureIndex);
-                    } else if (nextPictureIndex === 0) {
-                      setCurrentPicture(0);
-                    } else {
-                      setShowStared(false);
-                    }
-                  }
+    <>
+      <Email computerId={props.computerId} pictureSetId={pictureSetId} />
+      <Socket
+        host={`${basePath}?room=computer-${props.computerId}`}
+        registerId={props.computerId}
+        subscribeTopic="fetch-fresh-data"
+        subscribeCallback={(topic, data) => updateCollection()}
+      >
+        {() => (
+          <>
+            <PictureViewer imgSrc={picture && picture.name} />
+            <Navbar
+              next={next}
+              pictureCount={pictureCollection.pictures.length}
+              prev={prev}
+              showStared={showStared}
+              stared={picture && picture.stared}
+              staredCount={staredCollection.filter((p) => p.stared).length}
+              toggleStarPicture={() => {
+                starPicture({
+                  setId: pictureSetId,
+                  pictureId: picture._id,
+                  stared: !picture.stared,
                 })
-                .then(updateCollection)
-                .catch((err) => console.error(err));
-            }}
-            toggleShowStared={() => {
-              setCurrentPicture(0);
-              setShowStared(!showStared);
-            }}
-          />
-        </>
-      )}
-    </Socket>
+                  .then(() => {
+                    if (picture.stared) {
+                      const nextPictureIndex = currentPicture - 1;
+                      if (nextPictureIndex > 0) {
+                        setCurrentPicture(nextPictureIndex);
+                      } else if (nextPictureIndex === 0) {
+                        setCurrentPicture(0);
+                      } else {
+                        setShowStared(false);
+                      }
+                    }
+                  })
+                  .then(updateCollection)
+                  .catch((err) => console.error(err));
+              }}
+              toggleShowStared={() => {
+                setCurrentPicture(0);
+                setShowStared(!showStared);
+              }}
+            />
+          </>
+        )}
+      </Socket>
+    </>
   );
 }
